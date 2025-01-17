0d91cf24d3bbf43a8c532786a827125912745955
Kyouhei Hayama <kyouhei@cloudflare.com>
2025-01-17 13:35:08 +0900

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -60,18 +60,3 @@ export default {
 		return Response.json({ input: { audio: [] }, response });
 	},
 } satisfies ExportedHandler<Env>;
-
-//"https://github.com/Azure-Samples/cognitive-services-speech-sdk/raw/master/samples/cpp/windows/console/samples/enrollment_audio_katie.wav"
-//'https://github.com/burgil/whisper-test/raw/main/test1.mp3'
-//"https://storage.googleapis.com/ondoku3/media/8398451bedca05afe643d933554497b9264c96106490cf0617da961b.mp3"
-//"https://pub-09eb6533a1c846af8df578e9f16623f7.r2.dev/cf-video.mp4"
-//"https://pub-09eb6533a1c846af8df578e9f16623f7.r2.dev/cf-video.mp3"
-//"https://clrd.ninjal.ac.jp/csj/sound-f/aps-smp.mp3"
-//"https://clrd.ninjal.ac.jp/csj/sound-f/interview_aps-smp.mp3"
-//"https://www.nhk.or.jp/lesson/en/mp3/audio_lesson_06.mp3"
-//"https://www.city.takasaki.gunma.jp/uploaded/attachment/26016.mp3"
-//"https://pub-09eb6533a1c846af8df578e9f16623f7.r2.dev/pitch2023.mp3"
-//"https://cdn.pixabay.com/download/audio/2022/03/09/audio_f9f5c8899d.mp3?filename=japanese-conversation-23645.mp3"
-//"https://www.city.uonuma.lg.jp/uploaded/attachment/12268.mp3"
-//"https://www.sagamihara-shigikai.jp/doc/2024050900019/file_contents/a000008.mp3"
-
